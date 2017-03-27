5150f2ea14268d22db4030c79e0043eee10d753e
miracleant <chengfeng_hu@sian.cn>
2017-03-27 23:03:55 +0800

    index 界面修改完成，改进了缩进，减少了冗余的代码

--- index.php
+++ index.php
@@ -30,5 +30,488 @@
 		<div class="sidebar pc">
 					此处为简介或者广告位
 		</div>
+		<!-- 轮播器下方的照片展示位 -->
+		<div class="photo">
+			<div class="pc" style="width:940px;height:400px; ">
+				<div class="pc-photo" style="width:320px; height:400px; float:left;background:black;">
+				</div>
+				<div class="pc-photo" style="width:620px;height:200px; float:left;background:white;">
+				</div>
+				<div class="pc-photo" style="width:310px;height:200px;float:right;background:gray;">
+				</div>
+				<div class="pc-photo" style="width:310px;height:200px;float:right;background:green;">
+				</div>
+			</div>
+		</div>
+		<!-- 分界线 -->
+		<div class="word">
+			<a href="more.html" target="_blank" style="float:right;padding:3px; margin-bottom:5px;">更多</a>
+			<h2 style="font-size:16px; display；inline; margin-bottom:0px;">商品</h2>
+			<hr style="margin:0px; padding:0px;"/>	
+		</div>
+		<!-- 商品图片展示 -->
+		<div class="phone">	
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+		</div>
+<!-- 手机端图片展示到此为止，以下为电脑端的扩展 -->
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
+			<div class="photo-dis">
+				<p>
+						<a href="detail.html">
+							<img src="image/photo1.jpg" />
+						</a>
+				</p>
+				<div>
+					<h3 class="photo-word-dis">
+						<a href="detail.html">埃菲尔铁塔</a>
+					</h3>
+					<p class="photo-word photo-word-dis">
+						<span class="fense photo-word-dis">10</span>
+						 评论&nbsp
+						<span class="fense photo-word-dis">15</span>
+						 收藏&nbsp
+					</p>
+				</div>
+				<div>	
+					<dd style="float:right;margin-top:10px;">
+						<a href="#" style="font-size:16px;">paral<img src="image/photo3.jpg" style="width:28px;height:28px;padding:0px 5px;border-radius:14px;"></a>
+					</dd>
+				</div>
+			</div>
 	</div><!-- content结尾 -->
 <?php include('footer.php') ?>
